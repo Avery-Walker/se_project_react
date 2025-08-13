92c4cd9e38e1c4bc37140033fb55ab6582abe799
AveryWalker <aswalk2023@gmail.com>
2025-08-13 14:29:39 -0500

    Styling

---
+++ se_project_react/src/components/ModalWithForm/ModalWithForm.jsx
@@ -0,0 +1,25 @@
+import "./ModalWithForm.css";
+import x from "../../assets/x.svg";
+
+function ModalWithForm({ children, buttonText, title, activeModal, onClose }) {
+  return (
+    <div
+      className={`modal ${activeModal === "add-garment" ? "modal_opened" : ""}`}
+    >
+      <div className="modal__content">
+        <h2 className="modal__title">{title}</h2>
+        <button onClick={onClose} type="button" className="modal__close">
+          <img className="modal__close-btn" src={x} alt="close button" />
+        </button>
+        <form className="modal__form">
+          {children}
+          <button className="modal__submit" type="submit">
+            {buttonText}
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default ModalWithForm;
